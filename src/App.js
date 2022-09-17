d78bb7402313a7535fae490cc0f0d9aab5f74e5c
shaniyaclement <shaniyaclement17@gmail.com>
2022-09-16 19:29:41 -0500

    Adding scroll effect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
 
-function App() {
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+
+  const getMovieRequest = async () => {
+    const url = "http://www.omdbapi.com/?s=avengers&apikey=9cb76cd0";
+    
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    console.log(responseJson);
+    setMovies(responseJson.Search);
+  }; // end getMovieRequest()
+
+  useEffect(()=>{
+    getMovieRequest();
+  }, []); // end useEffect
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className= 'container-fluid movie-app'>
+      <div className='row'>
+        <MovieListHeading heading='Movies'/>
+      </div>
+      <div className='row'>
+        <MovieList movies={movies} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
